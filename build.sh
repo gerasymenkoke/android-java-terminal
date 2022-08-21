0275dd6cc0f67972d49463277ef48a42ff82da2b
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-08-21 21:38:09 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ $AAPT list $PROJ/bin/hello.unaligned.apk
 echo "Aligning and signing APK...."
 pwd
 ls -l
-$APKSIGNER sign --ks $PROJ/mykey.keystore $PROJ/bin/hello.apk
+$APKSIGNER sign --ks $PROJ/mykey.keystore  $PROJ/bin/hello.apk
 
 
 $ZIPALIGN -f 4 $PROJ/bin/hello.unaligned.apk bin/hello.apk
