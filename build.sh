4fc0371e2fa4bca3c84d5e24ff121ccd8a9ed3fb
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-08-25 22:34:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ cd $PROJ/bin
 pwd
 ls -l
 
-keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048 < echo 'B395b39595' < echo 'B395b39595'
+$ printf 'B395b39595\nB395b39595\n' | keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048 
 
 $APKSIGNER sign --ks mykey.keystore $PROJ/bin/hello.apk
 
