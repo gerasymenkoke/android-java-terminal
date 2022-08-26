3a26404ed347a06a9a421b49e29ce1103fc79ec2
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-08-26 15:20:52 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ cd $PROJ/bin
 pwd
 ls -l
 
-№ printf 'B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n' | keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048 
+# printf 'B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n' | keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048 
 printf 'B395b39595\n' | openssl base64 -in mykey.keystore 
 pwd
 ls -l
