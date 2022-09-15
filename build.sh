c135c543388013ce5f64e56b1946a47a3be6ead6
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-09-15 20:58:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,10 +16,6 @@ rm -rf src/com/example/helloandroid/R.java
 mkdir bin
 
 
-
-
-
-
 echo "Generating R.java file..."
 $AAPT package -f -m -J src -M AndroidManifest.xml -S res -I $PLATFORM
 
@@ -31,50 +27,18 @@ echo "Translating in Dalvik bytecode..."
 $DX --dex --output=classes.dex obj
 
 echo "Making APK..."
+$AAPT package -f -m -F $PROJ/bin/hello.unaligned.apk -M $PROJ/AndroidManifest.xml -S $PROJ/res -I $PLATFORM
+$AAPT add $PROJ/bin/hello.unaligned.apk classes.dex
 
-
-
-$AAPT package -f -m -F $PROJ/bin/hello.unaligned.aab -M $PROJ/AndroidManifest.xml -S $PROJ/res -I $PLATFORM
-$AAPT add $PROJ/bin/hello.unaligned.aab classes.dex
-
-$AAPT2 
-
-
-$AAPT list $PROJ/bin/hello.unaligned.aab
+$AAPT list $PROJ/bin/hello.unaligned.apk
 cd $PROJ/bin
 pwd
 ls -l
 cp hello.unaligned.aab  /home/runner/work/android-java-terminal/android-java-terminal/app/build/outputs/apk/release/
 cd /home/runner/work/android-java-terminal/android-java-terminal/app/build/outputs/apk/release
-# cp /home/runner/work/android-java-terminal/android-java-terminal/Kostya.jks  /home/runner/work/android-java-terminal/android-java-terminal/app/build/outputs/apk/release/signingKey.jks
-# cd /home/runner/work/android-java-terminal/android-java-terminal/app/build/outputs/apk/release
-pwd
-ls -l
-
-# printf 'B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n' | keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048 
-# pwd
-# ls -l
-# cp $PROJ/bin/mykey.keystore /usr/local/lib/android/sdk/build-tools/29.0.3/mykey.keystore
-#cd /usr/local/lib/android/sdk/build-tools/29.0.3
-#pwd
-# ls -l
-#chmod +x mykey.keystore
 
 
-#cd /opt/hostedtoolcache/Java_Adopt_jdk/18.0.2-9/x64
 
-
-#printf 'B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n' |  keytool -genkey -alias tomcat -keyalg RSA -keystore keystore.jks  
-
-#pwd
-#ls -l
-
-
-#$APKSIGNER sign --ks file:///opt/hostedtoolcache/Java_Adopt_jdk/18.0.2-9/x64keystore.jks $PROJ/bin/hello.apk
-#$APKSIGNER  verify -v --print-certs *.apk
-
-
-# $APKSIGNER sign --ks Kostya.jks $PROJ/bin/hello.apk
 
 
 
