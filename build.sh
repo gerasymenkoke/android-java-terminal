8628ec9ab7b25bb2740acdb77ba1ee495f6945dd
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-02 17:01:11 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -131,7 +131,7 @@ cd $PROJ/bin
 echo "jarsigner....."
 cat $PROJ/password.txt
 cat $PROJ/keystore.jks
-$JAVA_HOME/bin/jarsigner  -keystore $PROJ/keystore.jks      $PROJ/bin/hello.aab alias  < $PROJ/password.txt
+$JAVA_HOME/bin/jarsigner  -keystore $PROJ/keystore.jks      $PROJ/bin/hello.aab alias1  < $PROJ/password.txt
 
 
 
