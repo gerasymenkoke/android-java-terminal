32aac5fed5b6027810e1873d21bec6997353506d
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-02 12:56:11 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -56,8 +56,10 @@ ls -l
 
 echo "Signing  APK...."
 cd /home/runner/work/android-java-terminal/android-java-terminal/
-echo $PASSWORDB64 | base64 -d > /home/runner/work/android-java-terminal/android-java-terminal/password.txt
+# echo $PASSWORDB64 | base64 -d > /home/runner/work/android-java-terminal/android-java-terminal/password.txt
 echo $KEYSTORE_JKS| base64 -d > /home/runner/work/android-java-terminal/android-java-terminal/keystore.jks
+echo $PASSWORDB64 > $PROJ/password.txt
+cat $PROJ/password.txt
 cat $PROJ/keystore.jks
 echo "__________\n"
 echo "begin: signing_key.jks...."
