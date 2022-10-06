2d6b5298ab09e81cb0e5b6306750c57ded8cfab1
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-06 21:22:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -62,7 +62,7 @@ cd /home/runner/work/android-java-terminal/android-java-terminal/
 
 printf "B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n" | $JAVA_HOME/bin/keytool -genkeypair -validity 20000 -keystore signing_key.jks -keyalg RSA -alias alias2 -keysize 2048 
 
-printf "Yes\n" | $JAVA_HOME/bin/keytool -importcert -file deployment_cert.der    -keystore signing_key.jks -storepass "B395b39595"  -alias alias2
+printf "Yes\n" | $JAVA_HOME/bin/keytool -importcert -file deployment_cert.der    -keystore signing_key.jks -storepass "B395b39595"  
 
 
 
