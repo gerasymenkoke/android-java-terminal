1d673def69136f8a4bd885432808d6d8903cbcbb
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-09 20:17:59 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ pwd
 ls -l
 
 
-$JAVA_HOME/bin/java -jar $PROJ/pepk.jar --keystore=$PROJ/signing_key.jks --alias=alias --output=$PROJ/signing_key.pem  --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a <  "B395b39595"         
+printf "B395b39595\nB395b39595\n" | $JAVA_HOME/bin/java -jar $PROJ/pepk.jar --keystore=$PROJ/signing_key.jks --alias=alias --output=$PROJ/signing_key.pem  --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a        
 
 
 
