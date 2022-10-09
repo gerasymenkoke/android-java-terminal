b2e982b7cd33412e1af619bf7ced3a0d13aa9f33
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-09 21:04:40 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -144,7 +144,10 @@ printf   "B395b39595" > $PROJ/password.txt
 #cat $PROJ/password.txt
 
           
-$APKSIGNER sign  --ks-key-alias alias   $PROJ/signing_key.jks   $PROJ/app/build/outputs/apk/release/hello.apk          
+$APKSIGNER sign  --ks $PROJ/signing_key.jks   $PROJ/app/build/outputs/apk/release/hello.apk  --ks-key-alias alias
+
+# --ks my.keystore my-app.apk --ks-key-alias alias_name
+
 # $APKSIGNER verify -v  -v4-signature-file $PROJ/app/build/outputs/apk/release/hello.apk.idsig $PROJ/app/build/outputs/apk/release/hello.apk 
 
 
