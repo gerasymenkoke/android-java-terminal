6d9b7cfbcb2ab2c8ca5167ac2f1e36b347da0a6d
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-09 09:35:16 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -65,12 +65,12 @@ printf "B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n" | $JAVA_HOME/bin/keyto
 #printf "B395b39595\nB395b39595\nYes\n" | $JAVA_HOME/bin/keytool -importcert -file upload_cert.der -validity 20000 -keystore signing_key.jks -keyalg RSA -keysize 2048 
 
 
-$JAVA_HOME/bin/java -jar $PROJ/pepk.jar --keystore=$PROJ/signing_key.jks --alias=alias2 --output=$PROJ/signing_key.jks.zip --include-cert --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a
+printf "B395b39595\nB395b39595\nYes\n" | $JAVA_HOME/bin/java -jar $PROJ/pepk.jar --keystore=$PROJ/signing_key.jks --alias=alias2 --output=$PROJ/signing_key.jks.zip --include-cert --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a
 
 
 
 
-printf "B395b39595\nB395b39595\nYes\n" | $JAVA_HOME/bin/keytool -list -v -keystore $PROJ/signing_key.jks   -storepass "B395b39595"    
+ $JAVA_HOME/bin/keytool -list -v -keystore $PROJ/signing_key.jks   -storepass "B395b39595"    
 
 
 
