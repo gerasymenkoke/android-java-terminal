b68c81050b90881c16b4b65ccc79247831ac8aeb
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-09 12:36:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -59,7 +59,7 @@ cd /home/runner/work/android-java-terminal/android-java-terminal/
 
 
 
-printf "B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n" | $JAVA_HOME/bin/keytool -genkeypair -validity 20000 -keystore signing_key.jks -keyalg RSA -keysize 2048 
+printf "B395b39595\nB395b39595\nA\nB\nC\nD\nE\nUA\nYes\n" | $JAVA_HOME/bin/keytool -genkeypair -validity 20000 -keystore signing_key.jks -keyalg RSA -keysize 2048 -alias=alias
 
 #printf "Yes\n" | $JAVA_HOME/bin/keytool -importcert -file upload_cert.der    -keystore signing_key.jks -storepass "B395b39595"  
 #printf "B395b39595\nB395b39595\nYes\n" | $JAVA_HOME/bin/keytool -importcert -file upload_cert.der -validity 20000 -keystore signing_key.jks -keyalg RSA -keysize 2048 
@@ -71,7 +71,7 @@ pwd
 ls -l
 
 
-printf "B395b39595\n"  | $JAVA_HOME/bin/java -jar $PROJ/pepk.jar --keystore=$PROJ/signing_key.jks --alias=mykey --output=$PROJ/signing_key.pem --include-cert --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a
+printf "B395b39595\n"  | $JAVA_HOME/bin/java -jar $PROJ/pepk.jar --keystore=$PROJ/signing_key.jks --alias=alias --output=$PROJ/signing_key.pem --include-cert --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a
 
 
 
