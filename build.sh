8e1e9898edb5a9d0a00154d7e22493ddd7b7d186
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-10-22 20:23:02 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,11 +31,10 @@ echo "Compiling APK..."
 javac -d obj -classpath src -bootclasspath $PLATFORM -source 1.7 -target 1.7 src/com/example/helloandroid/MainActivity.java
 # javac -d obj -classpath src -bootclasspath $PLATFORM -source 1.7 -target 1.7 src/com/example/helloandroid/R.java
 
-
-javac  src/com/example/helloandroid/MainActivity.java
+cd obj
 pwd
 ls -l
-
+cd $PROJ
 
 
 echo "Translating in Dalvik bytecode..."
