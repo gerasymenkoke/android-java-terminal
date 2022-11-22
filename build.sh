3cbe4af1be77282769ff26dd2c818cc8d05e9bc3
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-11-22 21:09:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -64,7 +64,7 @@ echo "Making JAR..."
 cd $PROJ
 
 #jar cvf TicTacToe.jar TicTacToe.class audio images
-jar cfe  hello.jar obj src res
+jar cfe  hello.jar com.hellokostya.helloandroid obj src res
 pwd 
 ls -l
 
