58aa0cc8496085a238fff4bb967518f918284093
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-14 10:59:40 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -118,7 +118,7 @@ cd /home/runner/work/android-java-terminal/android-java-terminal/
 
 printf   "B395b39595" > $PROJ/password.txt
 # echo ${{ secrets.SIGNING_KEY_JKS }} > $PROJ/signing_key.jks
-base64 -d $PROJ/signing_key.jks > $PROJ/d_signing_key.jks
+base64  $PROJ/signing_key.jks > $PROJ/d_signing_key.jks
 echo d_signing_key.jks      
 cat  $PROJ/d_signing_key.jks          
 $APKSIGNER sign  --ks $PROJ/signing_key.jks   $PROJ/app/build/outputs/apk/release/hello.apk  < $PROJ/password.txt
